0a2923041ba2642f686088b6ad4fcee3cc5c1724
Nikolay Ukraintsev <ukrajncev@gmail.com>
2014-04-04 11:41:16 +0300

    Our leters in out put.

--- src/cap/simple/Main.java
+++ src/cap/simple/Main.java
@@ -3,6 +3,6 @@ package cap.simple;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("А тепер нашими!!! Привіт СВІТ!!!!");
     }
 }
